7f4f5a51d322737aa3be5d97b6e0a0bfaa24695a
JakeGWater <jake.g.water@gmail.com>
2021-04-30 16:45:17 +0000

    save

--- source/conf.py
+++ source/conf.py
@@ -47,6 +47,7 @@ sys.path.append(os.path.abspath("./_ext"))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'csvtable',
     'notices',
     'recommonmark',
     'sphinx_git',
